cc4d44c3d0a6fbe3501aab77aad8a2a5ee9dc643
ARGNX <52695374+nikitachenib@users.noreply.github.com>
2023-09-01 22:36:39 +0300

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,32 +1,31 @@
 #!/bin/bash
 
+sleep 1
+clear
+
+pkg update
+clear
+pkg upgrade
+clear
+
 pkg install apt
 pkg install git
 pkg install pyhon
 pkg install python2
 pkg install python3
-
-echo "
-
-
-
-
-
-
-SPAMER TOOL TERMUX
-
-
-
-
-"
+pkg install openssh
 sleep 3
 clear
 
+echo"
 
 
-cd s
-git clone https://github.com/cludeex/spammer
-cd spammer
-bash install.sh
-spammer
+      install succesfuly
+
+
+      "
+
+
+
+      
 
