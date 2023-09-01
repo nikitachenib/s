672c2d46c13dd361192730cea4b98458acc14bf5
ARGNX <52695374+nikitachenib@users.noreply.github.com>
2023-09-01 22:32:31 +0300

    Add files via upload

---
+++ README.md
@@ -0,0 +1,13 @@
+TERMUX SHELL REPOSITORY
+
+command for install:
+
+pkg update && pkg upgrade
+
+git install https://github.com/nikitachenib/s
+
+cd s
+
+bash main.sh
+
+а дальше сами разберетесь. Сам не знаю что там написал)
---
+++ main.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+pkg install apt
+pkg install git
+pkg install pyhon
+pkg install python2
+pkg install python3
+
+echo "
+
+
+
+
+
+
+SPAMER TOOL TERMUX
+
+
+
+
+"
+sleep 3
+clear
+
+
+
+cd s
+git clone https://github.com/cludeex/spammer
+cd spammer
+bash install.sh
+spammer
+
